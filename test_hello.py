7e2fdc9495ee32dd27b38000c7a8270c8879654f
Brave Jongen <bravejongenit@gmail.com>
2023-11-21 13:49:13 +0000

    adds cli tests

--- test_hello.py
+++ test_hello.py
@@ -1,1 +1,9 @@
-print("Testing from AWS Cloud9")
+from click.testing import CliRunner
+from hello import calculate
+
+
+def test_calculate():
+    runner = CliRunner()
+    result = runner.invoke(calculate, ['--value1', '1', '--value2', '2'])
+    assert result.exit_code == 0
+    assert '2' in result.output
